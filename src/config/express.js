8a62b0dee145c5c3a020212006191d5217013a55
Ankit Duhan <ankit@progxtechnologies.com>
2023-03-07 20:44:35 +0530

    cors package added

--- src/config/express.js
+++ src/config/express.js
@@ -1,11 +1,12 @@
 const { json, urlencoded } = require("body-parser");
 
 const express = require("express");
+const cors = require("cors");
 const { serve, setup } = require("swagger-ui-express");
 const { connectDB } = require("./db.js");
 const routes = require("../routes/v1/index.js");
 const swaggerDocument = require("../../swagger.json");
-const cors = require("cors");
+
 const app = express();
 app.use(cors());
 
@@ -20,11 +21,11 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,x-access-token,Accept,Authorization",
+    "Origin,X-Requested-With,Content-Type,x-access-token,Accept,Authorization"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET,POST,PATCH,PUT,DELETE,OPTIONS",
+    "GET,POST,PATCH,PUT,DELETE,OPTIONS"
   );
   next();
 });
@@ -34,11 +35,13 @@ app.use("/api-doc", serve, setup(swaggerDocument));
 app.use("/v1", routes);
 
 // eslint-disable-next-line no-unused-vars
-app.use((req, res, next) => res.status(404).json({
-  code: 404,
-  errorMessage: "URL not found",
-  isPublic: true,
-}));
+app.use((req, res, next) =>
+  res.status(404).json({
+    code: 404,
+    errorMessage: "URL not found",
+    isPublic: true,
+  })
+);
 
 // eslint-disable-next-line no-unused-vars
 app.use((error, req, res, next) => {
