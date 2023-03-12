deefb0ecb9a5be3a06897abd853c4ee5d5a68c66
Ankit Singh Duhan <ankit@progxtechnologies.com>
2023-03-12 18:22:19 +0530

    test added

--- src/utils/mails.utils.js
+++ src/utils/mails.utils.js
@@ -32,12 +32,6 @@ async function sendMail(mailOptions) {
   });
 }
 
-exports.sendEmailVerificationMailTest = async (userEmail, token, role) {
-  return `<p>Welcome to SkillApp- you're almost ready to get started with your account!</p>
-  <p> But first, we need to verify your email. Please click <a href=${redirectionBaseUrl}auth/verify-email/${token}?role=${role}>on this link</a> to verify your email.</p>
-  <p>If you did not request to create a SkillApp account, please ignore this email!</p>`;
-}
-
 exports.sendEmailVerificationMail = async (userEmail, token, role) => {
   const mailOptions = {
     to: `${userEmail}`, // Change to your recipient
@@ -49,6 +43,14 @@ exports.sendEmailVerificationMail = async (userEmail, token, role) => {
   };
   await sendMail(mailOptions);
 };
+
+exports.sendEmailVerificationMailTest = (
+  userEmail,
+  token,
+  role
+) => `<p>Welcome to SkillApp- you're almost ready to get started with your account!</p>
+  <p> But first, we need to verify your email. Please click <a href=${redirectionBaseUrl}auth/verify-email/${token}?role=${role}>on this link</a> to verify your email.</p>
+  <p>If you did not request to create a SkillApp account, please ignore this email!</p>`;
 
 exports.sendInvitationEmail = async (userEmail, inviterName, userId) => {
   const mailOptions = {
