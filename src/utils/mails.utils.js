5d5d6ea1a092c59e6dcf7a930a3243f79a0ae9fb
Ankit Singh Duhan <ankit@progxtechnologies.com>
2023-03-12 13:56:45 +0530

    url updated

--- src/utils/mails.utils.js
+++ src/utils/mails.utils.js
@@ -38,7 +38,7 @@ exports.sendEmailVerificationMail = async (userEmail, token, role) => {
     from: smtpSenderEmail, // sender address
     subject: "Email Verification Mail",
     html: `<p>Welcome to SkillApp- you're almost ready to get started with your account!</p>
-            <p> But first, we need to verify your email. Please click <a href=${redirectionBaseUrl}user/auth/verify-email/${token}?role=${role}>on this link</a> to verify your email.</p>
+            <p> But first, we need to verify your email. Please click <a href=${redirectionBaseUrl}auth/verify-email/${token}?role=${role}>on this link</a> to verify your email.</p>
             <p>If you did not request to create a SkillApp account, please ignore this email!</p>`,
   };
   await sendMail(mailOptions);
@@ -50,7 +50,7 @@ exports.sendInvitationEmail = async (userEmail, inviterName, userId) => {
     from: smtpSenderEmail, // sender address
     subject: "Invitation-Register on SkillApp",
     html: `<p>You have been invited to join SkillApp by ${inviterName}. To accept the invitation, please click on the following link:</p>
-            <p> <a href=${redirectionBaseUrl}/invite/${userId}>on this link</a> to complete your registration process.</p>
+            <p> <a href=${redirectionBaseUrl}auth/invite/${userId}>on this link</a> to complete your registration process.</p>
             <p>If you do not want to create a SkillApp account, please ignore this email!</p>`,
   };
   await sendMail(mailOptions);
@@ -63,7 +63,7 @@ exports.sendForgotPasswordMail = async (email, resetPasswordToken) => {
     subject: "Reset your password on Your App",
     text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
-        https://${redirectionBaseUrl}/reset-password/${resetPasswordToken}\n\n
+        https://${redirectionBaseUrl}auth/reset-password/${resetPasswordToken}\n\n
         If you did not request this, please ignore this email and your password will remain unchanged.\n`,
   };
   await sendMail(mailOptions);
