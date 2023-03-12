0fe76a14a0c8480d906275951675b1e2d2345e56
Ankit Singh Duhan <ankit@progxtechnologies.com>
2023-03-12 12:52:14 +0530

    Env updated

--- src/utils/security.js
+++ src/utils/security.js
@@ -4,10 +4,17 @@ const { envVariables } = require("../config/vars");
 // eslint-disable-next-line arrow-body-style
 exports.encryptGivenText = (password) => {
   console.log(password, envVariables.passwordEncryptionKey);
-  return CryptoJS.AES.encrypt(password, envVariables.passwordEncryptionKey).toString();
+  return CryptoJS.AES.encrypt(
+    password,
+    envVariables.passwordEncryptionKey
+  ).toString();
 };
 
 exports.decryptGivenText = (password) => {
-  const bytes = CryptoJS.AES.decrypt(password, envVariables.passwordEncryptionKey);
+  console.log(password, envVariables.passwordEncryptionKey);
+  const bytes = CryptoJS.AES.decrypt(
+    password,
+    envVariables.passwordEncryptionKey
+  );
   return bytes.toString(CryptoJS.enc.Utf8);
 };
