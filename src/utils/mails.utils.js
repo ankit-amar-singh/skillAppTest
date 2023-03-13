7b0ac9df79c02fe86e56ca456256357fa529f926
Ankit Singh Duhan <ankit@progxtechnologies.com>
2023-03-13 20:06:39 +0530

    https removed

--- src/utils/mails.utils.js
+++ src/utils/mails.utils.js
@@ -70,7 +70,7 @@ exports.sendForgotPasswordMail = async (email, resetPasswordToken) => {
     subject: "Reset your password on Your App",
     text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
-        https://${redirectionBaseUrl}auth/reset-password/${resetPasswordToken}\n\n
+        ${redirectionBaseUrl}auth/reset-password/${resetPasswordToken}\n\n
         If you did not request this, please ignore this email and your password will remain unchanged.\n`,
   };
   await sendMail(mailOptions);
